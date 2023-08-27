f1b414102f7f84a91b97f8567334b5d5393e415d
abbey-glitch <abiodunonaolapi@gmail.com>
2023-08-27 12:48:53 +0100

    file conversion with encoding issues fixed

--- api.py
+++ api.py
@@ -14,21 +14,30 @@ feedbacks = datas.json()
 country = ''
 name = ''
 domains = ''
+web_pages = ''
 for data in feedbacks:
     country += data['country']
     name += data['name']
-    domains = data['domains'][0]
-    web_pages = data['web_pages'][0]
-    print(web_pages)
-    if cursor:
-       query = "INSERT INTO `unitab` VALUES (%s, %s, %s, %s, %s)"
-       values = ('', country, name, domains, web_pages)
-       if db_conn.is_connected():
-           cursor.execute(query, values)
-           db_conn.commit()
-           info = db_conn.get_server_info()
-           print('database created ' + info)
-       else:
-           print('unable to create data')
-    else:
-        print('off')
+    domains += data['domains'][0]
+    web_pages += data['web_pages'][0]
+    # if cursor:
+    #    query = "INSERT INTO `unitab` VALUES (%s, %s, %s, %s, %s)"
+    #    values = ('', country, name, domains, web_pages)
+    #    if db_conn.is_connected():
+    #        cursor.execute(query, values)
+    #        db_conn.commit()
+    #        info = db_conn.get_server_info()
+    #        print('database created ' + info)
+    #    else:
+    #        print('unable to create data')
+    # else:
+    #     print('off')
+
+# print(web_pages)
+# write_pdf = open("pag.pdf", 'w')
+# note = write_pdf.write(web_pages)
+# write_pdf.close()
+print(web_pages)
+write_pdf = open("nom.pdf", 'w', encoding='utf-8')
+note = write_pdf.write(name)
+write_pdf.close()
